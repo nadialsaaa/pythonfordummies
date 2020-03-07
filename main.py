434d59ee281ba46e5fcfd3a43f2ef165d50c9e72
Nadia Salsabila Herawati Putri <putrinadia5@gmail.com>
2020-03-07 09:25:43 +0700

    edit fungsi

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 def penjumlahan(a,b):
 	#isi fungsi penjumlahan diletakkan di variable total
+        total = a + b
 	return total
 
 def pengurangan(a,b):
 	#isi fungsi pengurangan diletakkan di variable total
-	return total
+	total = a - b
+        return total
 
 def perkalian(a,b):
 	#isi fungsi perkalian diletakkan di variable total 
-	return total
+	total = a * b
+        return total
 
 def pembagian(a,b):
 	#isi fungsi pembagian diletakkan di variable total
-	return total
+	total = a / b
+        return total
 
 def main():
 	print penjumlahan(5,5)
